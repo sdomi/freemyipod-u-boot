837957a441c99b9ee1784af1d86a72a60dfbb2b9
Serge Bazanski <q3k@q3k.org>
2023-02-21 17:37:05 +0100

    shell.nix: pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-with import <nixpkgs> {};
+let
+  nixpkgsCommit = "15b85dedcbaf9997bea11832106adb2195486443";
+  nixpkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsCommit}.tar.gz";
+    sha256 = "sha256:1s2ih6ch5rhz1m3s9vkbxrdvvmxvxkmyck9zyc3q87gg3hsn10jb";
+  };
+  pkgs = import nixpkgsSrc {};
+in
 
 pkgs.mkShell {
   packages = with pkgs; [
